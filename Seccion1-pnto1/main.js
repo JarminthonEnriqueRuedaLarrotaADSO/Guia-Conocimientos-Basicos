5adc0269eed464343c5fb86ab1d78c044595b7da
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-11-15 12:04:40 -0500

    Seccion1-pnto1

---
+++ Seccion1-pnto1/main.js
@@ -0,0 +1,24 @@
+addEventListener("DOMContentLoaded", (e)=> {
+    let formulario = document.querySelector('#form1')
+    let formulario2 = document.querySelector('#form2')
+    
+    formulario.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let datos = Object.fromEntries(new FormData(e.target))
+        let vDolar = parseFloat(4800)
+        let input = datos.dolar
+        let operacion = vDolar*input
+        dolares.innerHTML = `${operacion} Pesos`
+        
+    })
+    formulario2.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let datos = Object.fromEntries(new FormData(e.target))
+        let vPeso = 0.00021
+        let input = datos.peso
+        let operacion = input*vPeso
+        pesos.innerHTML = `${operacion} Dolares`
+    })
+    
+    })
+    
