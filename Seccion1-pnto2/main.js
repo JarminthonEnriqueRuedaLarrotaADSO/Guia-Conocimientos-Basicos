02bcbb816fa9de42b178058e7e0a9cce8230d239
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-11-15 12:06:27 -0500

    seccion2-pnto2

---
+++ Seccion1-pnto2/main.js
@@ -0,0 +1,14 @@
+addEventListener("DOMContentLoaded", (e)=> {
+    let formulario = document.querySelector('#form1')
+    
+    formulario.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let datos = Object.fromEntries(new FormData(e.target))
+        let gCelsius = datos.grados
+        let operacion = (gCelsius*9/5)+32
+        grados1.innerHTML = `${operacion} Grados Farenheit en New York..`
+        hola.innerHTML = operacion
+    })
+    
+    })
+    
