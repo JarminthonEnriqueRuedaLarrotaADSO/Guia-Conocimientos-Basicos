4d37d20df45c32e02cfb373e9b9ffee2405e2b48
JaminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-11-17 20:17:13 -0500

    Seccion1-pnto4

---
+++ Seccion1-pnto4/main.js
@@ -0,0 +1,18 @@
+addEventListener("DOMContentLoaded", (e)=>{
+    let form=document.querySelector('#form') 
+    let contadorPasos = 1
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault();       
+        let datos = Object.fromEntries(new FormData(e.target))
+        let input = datos.arroz
+        let tabla =document.querySelector("tbody");
+        tabla.insertAdjacentHTML("beforeend",`
+                <tr>
+                    <td> ${contadorPasos}  </td>
+                    <td> ${input}  </td>
+                </tr>
+                `)
+            contadorPasos++
+        
+    })               
+})
