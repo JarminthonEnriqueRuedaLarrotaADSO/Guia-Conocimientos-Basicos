ea09615c31a5f90326694a676241b93c14d74fd0
JaminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-12-05 18:41:26 -0500

    seccion2-pnto4

--- seccion2-pnto1/main.js
+++ seccion2-pnto1/main.js
@@ -4,13 +4,10 @@ let llamar = document.querySelector('#form')
 llamar.addEventListener("submit", (e)=>{
 e.preventDefault()
 let datos = Object.fromEntries(new FormData(e.target))
-let input = datos.kilometros
-let input1 = datos.minutos
-let operacion1 = input1*60
-let operacion = 60/input
-let decimal = operacion.toFixed(7)
-respues.innerHTML = `su resultado es ${decimal} m/km`
-respuesta.innerHTML = `su resultado es ${operacion1} km/h`
+let input = datos.grados_centigrados
+let operacion = (9/5*input) + 32
+
+respuesta.innerHTML = ` ${operacion} Grados Farenheit`
 
 })
 })
